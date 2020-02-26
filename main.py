1e12eb695738066169487c9499b59d28de84a79a
Erik Lindebratt <erik.lindebratt@gmail.com>
2020-02-26 11:37:40 +0100

    Set volume for configured devices before playing

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import fliclib
 import caster
 import logging
 import sys
+import os
 
 BLACK_BUTTON_ADDRESS = '80:e4:da:70:32:3b'
 TURQUOISE_BUTTON_ADDRESS = '80:e4:da:73:70:72'
@@ -14,6 +15,62 @@ castDevice = None
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+devicesToSetVolumeFor = os.environ.get('DEVICES_TO_SET_VOLUME_FOR')
+deviceToCastTo = os.environ.get('DEVICE_TO_CAST_TO')
+
+if devicesToSetVolumeFor is not None:
+    try:
+        devicesToSetVolumeFor = [
+            {
+                'device': caster.getDevice(a[0]),
+                'volume': float(a[1])
+            } for a in [
+                [
+                    n.strip() for n in i.strip().split('=')
+                ] for i in devicesToSetVolumeFor.split(',')
+            ]
+        ]
+    except Exception as e:
+        logger.error(
+            'Failed to configure devices to set volume for from environment: {}'
+                .format(e)
+        )
+        raise e
+
+def playOrStop():
+    global castDevice
+
+    if deviceToCastTo is None:
+        logger.error(
+            'Can\'t play or stop - no target device configured in env vars'
+        )
+        return
+
+    if castDevice is not None and caster.isPlaying(castDevice):
+        logger.info('currently playing - stopping')
+        caster.stop(castDevice)
+        caster.quit(castDevice)
+    else:
+        if devicesToSetVolumeFor is not None:
+            [caster.setVolume(
+                i['device'],
+                i['volume']
+            ) for i in devicesToSetVolumeFor]
+
+        castDevice = caster.play({
+            'deviceName': deviceToCastTo,
+            'media': {
+                'url': 'https://sverigesradio.se/topsy/direkt/srapi/132.mp3',
+                'args': {
+                    'stream_type': 'LIVE',
+                    'autoplay': True,
+                    'title': 'P1',
+                    'thumb': 'https://static-cdn.sr.se/sida/images/132/2186745_512_512.jpg?preset=api-default-square'
+                }
+            }
+        }, castDevice)
+
+
 def onButtonClick(channel, clickType, wasQueued, timeDiff):
     global castDevice
 
@@ -22,46 +79,10 @@ def onButtonClick(channel, clickType, wasQueued, timeDiff):
 
     if channel.bd_addr == BLACK_BUTTON_ADDRESS:
         logger.info('black button clicked')
-
-        if castDevice is not None and caster.isPlaying(castDevice):
-            logger.info('currently playing - stopping')
-            caster.stop(castDevice)
-            caster.quit(castDevice)
-        else:
-            castDevice = caster.play({
-                'deviceName': 'Home Group',
-                'volume': 0.2,
-                'media': {
-                    'url': 'https://sverigesradio.se/topsy/direkt/srapi/132.mp3',
-                    'args': {
-                        'stream_type': 'LIVE',
-                        'autoplay': True,
-                        'title': 'P1',
-                        'thumb': 'https://static-cdn.sr.se/sida/images/132/2186745_512_512.jpg?preset=api-default-square'
-                    }
-                }
-            }, castDevice)
+        playOrStop()
     elif channel.bd_addr == TURQUOISE_BUTTON_ADDRESS:
         logger.info('turqouise button clicked')
-
-        if castDevice is not None and caster.isPlaying(castDevice):
-            logger.info('currently playing - stopping')
-            caster.stop(castDevice)
-            caster.quit(castDevice)
-        else:
-            castDevice = caster.play({
-                'deviceName': 'Home Group',
-                'volume': 0.2,
-                'media': {
-                    'url': 'https://sverigesradio.se/topsy/direkt/srapi/132.mp3',
-                    'args': {
-                        'stream_type': 'LIVE',
-                        'autoplay': True,
-                        'title': 'P1',
-                        'thumb': 'https://static-cdn.sr.se/sida/images/132/2186745_512_512.jpg?preset=api-default-square'
-                    }
-                }
-            }, castDevice)
+        playOrStop()
 
 def onConnectionStatusChanged(channel, connection_status, disconnect_reason):
     logger.debug(channel.bd_addr + " " + str(connection_status) + (" " + str(disconnect_reason) if connection_status == fliclib.ConnectionStatus.Disconnected else ""))
