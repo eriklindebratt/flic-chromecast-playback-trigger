7730055f6523d48d913191a2f24aee5bb3ebc6b8
Erik Lindebratt <erik.lindebratt@gmail.com>
2020-03-17 01:20:44 +0100

    Working draft

--- util.py
+++ util.py
@@ -1,9 +1,23 @@
+import socket
+
 def formatTimeDelta(delta):
     hours, remainder = divmod(delta.seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
+
     return '{}h{}m{}s'.format(
         int(hours),
         int(minutes),
         int(seconds)
     )
+
+def getLocalIpAddress():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 80))
+
+    ipAddress = s.getsockname()[0]
+
+    s.shutdown(socket.SHUT_RDWR)
+    s.close()
+
+    return ipAddress
 
